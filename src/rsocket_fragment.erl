67baeeb91c5af50982d8585c5d37019db4ca63e2
Markus Flambard <markus@flambard.se>
2021-05-23 20:18:18 +0200

    Fragmentation planner for REQUEST_FNF frames and refactoring

--- src/rsocket_fragment.erl
+++ src/rsocket_fragment.erl
@@ -2,48 +2,37 @@
 
 -export([
          plan_request_response/2,
+         plan_request_fnf/2,
          plan_payload/2
         ]).
 
 -define(PAYLOAD_SPACE, 16777209).
 -define(REQUEST_RESPONSE_SPACE, 16777209).
+-define(REQUEST_FNF_SPACE, 16777209).
 
-%%%
-%%% REQUEST_RESPONSE
-%%%
-
-plan_request_response(MetadataLength, DataLength)
-  when MetadataLength > ?REQUEST_RESPONSE_SPACE ->
-    Remaining = MetadataLength - ?REQUEST_RESPONSE_SPACE,
-    Following = plan_payload(Remaining, DataLength),
-    [{request_response, ?REQUEST_RESPONSE_SPACE, 0} | Following];
-
-plan_request_response(MetadataLength, DataLength)
-  when MetadataLength + DataLength > ?REQUEST_RESPONSE_SPACE ->
-    DataFragmentLength = ?REQUEST_RESPONSE_SPACE - MetadataLength,
-    Remaining = DataLength - DataFragmentLength,
-    Following = plan_payload(0, Remaining),
-    [{request_response, MetadataLength, DataFragmentLength} | Following];
 
 plan_request_response(MetadataLength, DataLength) ->
-    [{request_response, MetadataLength, DataLength}].
+    plan(request_response, ?REQUEST_RESPONSE_SPACE, MetadataLength, DataLength).
 
-
-%%%
-%%% PAYLOAD
-%%%
-
-plan_payload(MetadataLength, DataLength) when MetadataLength > ?PAYLOAD_SPACE ->
-    Remaining = MetadataLength - ?PAYLOAD_SPACE,
-    Following = plan_payload(Remaining, DataLength),
-    [{payload, ?PAYLOAD_SPACE, 0} | Following];
-
-plan_payload(MetadataLength, DataLength)
-  when MetadataLength + DataLength > ?PAYLOAD_SPACE ->
-    DataFragmentLength = ?PAYLOAD_SPACE - MetadataLength,
-    Remaining = DataLength - DataFragmentLength,
-    Following = plan_payload(0, Remaining),
-    [{payload, MetadataLength, DataFragmentLength} | Following];
+plan_request_fnf(MetadataLength, DataLength) ->
+    plan(request_fnf, ?REQUEST_FNF_SPACE, MetadataLength, DataLength).
 
 plan_payload(MetadataLength, DataLength) ->
-    [{payload, MetadataLength, DataLength}].
+    plan(payload, ?PAYLOAD_SPACE, MetadataLength, DataLength).
+
+
+plan(Request, Space, MetadataLength, DataLength)
+  when MetadataLength > Space ->
+    Remaining = MetadataLength - Space,
+    Following = plan(payload, ?PAYLOAD_SPACE, Remaining, DataLength),
+    [{Request, Space, 0} | Following];
+
+plan(Request, Space, MetadataLength, DataLength)
+  when MetadataLength + DataLength > Space ->
+    DataFragmentLength = Space - MetadataLength,
+    Remaining = DataLength - DataFragmentLength,
+    Following = plan(payload, ?PAYLOAD_SPACE, 0, Remaining),
+    [{Request, MetadataLength, DataFragmentLength} | Following];
+
+plan(Request, _Space, MetadataLength, DataLength) ->
+    [{Request, MetadataLength, DataLength}].
