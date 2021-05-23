f0c7dcc05afdacc516fc528d98bef41389ec07fd
Markus Flambard <markus@flambard.se>
2021-05-23 20:02:50 +0200

    Fragmentation planner for REQUEST_RESPONSE frames

--- src/rsocket_fragment.erl
+++ src/rsocket_fragment.erl
@@ -1,10 +1,37 @@
 -module(rsocket_fragment).
 
 -export([
+         plan_request_response/2,
          plan_payload/2
         ]).
 
 -define(PAYLOAD_SPACE, 16777209).
+-define(REQUEST_RESPONSE_SPACE, 16777209).
+
+%%%
+%%% REQUEST_RESPONSE
+%%%
+
+plan_request_response(MetadataLength, DataLength)
+  when MetadataLength > ?REQUEST_RESPONSE_SPACE ->
+    Remaining = MetadataLength - ?REQUEST_RESPONSE_SPACE,
+    Following = plan_payload(Remaining, DataLength),
+    [{request_response, ?REQUEST_RESPONSE_SPACE, 0} | Following];
+
+plan_request_response(MetadataLength, DataLength)
+  when MetadataLength + DataLength > ?REQUEST_RESPONSE_SPACE ->
+    DataFragmentLength = ?REQUEST_RESPONSE_SPACE - MetadataLength,
+    Remaining = DataLength - DataFragmentLength,
+    Following = plan_payload(0, Remaining),
+    [{request_response, MetadataLength, DataFragmentLength} | Following];
+
+plan_request_response(MetadataLength, DataLength) ->
+    [{request_response, MetadataLength, DataLength}].
+
+
+%%%
+%%% PAYLOAD
+%%%
 
 plan_payload(MetadataLength, DataLength) when MetadataLength > ?PAYLOAD_SPACE ->
     Remaining = MetadataLength - ?PAYLOAD_SPACE,
