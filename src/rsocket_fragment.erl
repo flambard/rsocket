5fe34db41b4715e0cce945345c669700fdc1f129
Markus Flambard <markus@flambard.se>
2021-05-23 20:37:58 +0200

    Fragmentation planner for REQUEST_CHANNEL frames

--- src/rsocket_fragment.erl
+++ src/rsocket_fragment.erl
@@ -4,6 +4,7 @@
          plan_request_response/2,
          plan_request_fnf/2,
          plan_request_stream/2,
+         plan_request_channel/2,
          plan_payload/2
         ]).
 
@@ -11,6 +12,7 @@
 -define(REQUEST_RESPONSE_SPACE, 16777209).
 -define(REQUEST_FNF_SPACE, 16777209).
 -define(REQUEST_STREAM_SPACE, 16777205).
+-define(REQUEST_CHANNEL_SPACE, 16777205).
 
 
 plan_request_response(MetadataLength, DataLength) ->
@@ -21,6 +23,9 @@ plan_request_fnf(MetadataLength, DataLength) ->
 
 plan_request_stream(MetadataLength, DataLength) ->
     plan(request_stream, ?REQUEST_STREAM_SPACE, MetadataLength, DataLength).
+
+plan_request_channel(MetadataLength, DataLength) ->
+    plan(request_channel, ?REQUEST_CHANNEL_SPACE, MetadataLength, DataLength).
 
 plan_payload(MetadataLength, DataLength) ->
     plan(payload, ?PAYLOAD_SPACE, MetadataLength, DataLength).
---
+++ test/rsocket_fragment_request_channel_tests.erl
@@ -0,0 +1,45 @@
+-module(rsocket_fragment_request_channel_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+small_request_channel_no_metadata_test() ->
+    MetadataLength = 0,
+    DataLength = 2000,
+    [{request_channel, MetadataLength, DataLength}] =
+        rsocket_fragment:plan_request_channel(MetadataLength, DataLength).
+
+small_request_channel_small_metadata_test() ->
+    MetadataLength = 2500,
+    DataLength = 1800,
+    [{request_channel, MetadataLength, DataLength}] =
+        rsocket_fragment:plan_request_channel(MetadataLength, DataLength).
+
+large_request_channel_small_metadata_test() ->
+    MetadataLength = 2500,
+    DataLength = 20000000,
+    [
+     {request_channel, MetadataLength, DataLength1},
+     {payload, 0, DataLength2}
+    ] = rsocket_fragment:plan_request_channel(MetadataLength, DataLength),
+    DataLength = DataLength1 + DataLength2.
+
+small_request_channel_large_metadata_test() ->
+    MetadataLength = 25000000,
+    DataLength = 3400,
+    [
+     {request_channel, MetadataLength1, 0},
+     {payload, MetadataLength2, DataLength}
+    ] = rsocket_fragment:plan_request_channel(MetadataLength, DataLength),
+    MetadataLength = MetadataLength1 + MetadataLength2.
+
+large_request_channel_large_metadata_test() ->
+    MetadataLength = 25000000,
+    DataLength = 34000000,
+    [
+     {request_channel, MetadataLength1, 0},
+     {payload, MetadataLength2, DataLength1},
+     {payload, 0, DataLength2},
+     {payload, 0, DataLength3}
+    ] = rsocket_fragment:plan_request_channel(MetadataLength, DataLength),
+    MetadataLength = MetadataLength1 + MetadataLength2,
+    DataLength = DataLength1 + DataLength2 + DataLength3.
